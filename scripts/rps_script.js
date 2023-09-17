02e513d4bc9aaa68025a85174745cec26a4fd890
Dimm005 <dimm005@gmail.com>
2023-09-17 14:51:10 +0800

    Add two functions to rps_script

--- scripts/rps_script.js
+++ scripts/rps_script.js
@@ -2,14 +2,49 @@ function getComputerChoice () {
     let intChoice = Math.floor(Math.random() * 3);
     switch (intChoice) {
         case 0:
-            return 'Rock';
+            return 'rock';
         case 1:
-            return 'Paper';
+            return 'paper';
         case 2:
-            return 'Scissors';
+            return 'scissors';
         default:
             return 'Choice Error!';
     }
 }
 
-console.log(getComputerChoice())
+function playRound (playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case 'rock':
+            if (computerSelection == 'rock') {
+                return 'Draw! Rock vs rock!';
+            } else if (computerSelection == 'paper') {
+                return 'You lose! Paper covers rock!';
+            } else {
+                return 'You won! Rock beats scissors!'
+            };
+        case 'paper':
+            if (computerSelection == 'paper') {
+                return 'Draw! Paper vs paper!';
+            } else if (computerSelection == 'scissors') {
+                return 'You lose! Scissors cuts paper!';
+            } else {
+                return 'You won! Paper covers rock!'
+            };
+        default:
+            if (computerSelection == 'scissors') {
+                return 'Draw! Scissors vs scissors!';
+            } else if (computerSelection == 'rock') {
+                return 'You lose! Rock beats scissors!';
+            } else {
+                return 'You won! Scissors cuts paper!'
+            };
+
+
+    }
+}
+
+
+let playerSelection = 'paper';
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
